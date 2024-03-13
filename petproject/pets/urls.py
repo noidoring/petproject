8d9f364de82ac2f98a1c0dd71e15052db50366fc
Rodion <noidoring@gmail.com>
2024-03-13 15:20:45 +0300

    add name to first path

--- petproject/pets/urls.py
+++ petproject/pets/urls.py
@@ -6,7 +6,7 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index), 
+    path('', views.index, name='home'), 
     path('cats/<int:cat_id>/', views.categories),
     path('cats/<slug:cat_slug>', views.categories_by_slug),
     path('archive/<year4:year>/', views.archive),
