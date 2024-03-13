bb08631fd1ef994b72afa50e2587900e9062cd58
Rodion <noidoring@gmail.com>
2024-03-13 13:47:43 +0300

    add function of view page_not_found

--- petproject/pets/views.py
+++ petproject/pets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
     return HttpResponse('main')
@@ -15,3 +15,7 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     return HttpResponse(f'{year}')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
