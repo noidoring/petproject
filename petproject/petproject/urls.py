a004c5cabad89548229c42d0a31e3b22aab120d1
Rodion <noidoring@gmail.com>
2024-03-13 13:48:13 +0300

    add handler404

--- petproject/petproject/urls.py
+++ petproject/petproject/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from pets.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pets.urls')),
 ]
+
+
+handler404 = page_not_found
