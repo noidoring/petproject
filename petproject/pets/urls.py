f78b28e542998aced2b471369ea4510b5222e275
Rodion <noidoring@gmail.com>
2024-03-27 22:47:30 +0300

    delete archive path

--- petproject/pets/urls.py
+++ petproject/pets/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('about/', views.about),
     path('cats/<int:cat_id>/', views.categories),
     path('cats/<slug:cat_slug>', views.categories_by_slug),
-    path('archive/<year4:year>/', views.archive),
     path('addpost/', views.addpost),
     path('contact/', views.contact),
 ]
