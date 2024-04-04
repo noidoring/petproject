2f44f94b17864eab89133b1530f91cf39fe4c9b5
Rodion <noidoring@gmail.com>
2024-04-04 13:24:27 +0300

    update model Pets

--- petproject/pets/models.py
+++ petproject/pets/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class Pets(models.Model):
+    title = models.CharField(max_length=200)
+    from_where = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
     
